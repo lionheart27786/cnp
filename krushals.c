b070c44234cd608632aff54ae7baafaed3c61c79
Archit-Gupta-CSE <91718724+Archit-Gupta-CSE@users.noreply.github.com>
2024-08-18 16:42:03 +0530

    Update krushals.c

--- krushals.c
+++ krushals.c
@@ -12,31 +12,24 @@ struct edge {
 int edge_count = 0;
 
 void getdata(int index, int total) {
-    int i;
-    for (i = index; i < total; i++) {
-        if (i != index) {
-            printf("\nEnter distance between Vertex %c and %c:", index + 65, i + 65);
-            scanf("%d", &e[edge_count].distance);
-            e[edge_count].first_node = index;
-            e[edge_count].second_node = i;
-            ++edge_count;
-        }
+    for (int i = index + 1; i < total; i++) {
+        printf("\nEnter distance between Vertex %c and %c:", index + 65, i + 65);
+        scanf("%d", &e[edge_count].distance);
+        e[edge_count].first_node = index;
+        e[edge_count].second_node = i;
+        ++edge_count;
     }
 }
 
 void init(int total) {
-    int i;
-    for (i = 0; i < total; i++)
-        node[i].set = i;
-    for (i = 0; i < edge_count; i++)
-        e[i].selected = -1;
+    for (int i = 0; i < total; i++) node[i].set = i;
+    for (int i = 0; i < edge_count; i++) e[i].selected = -1;
 }
 
 void sort() {
-    int i, j;
     struct edge temp;
-    for (i = 0; i < edge_count - 1; i++) {
-        for (j = 0; j < edge_count - i - 1; j++) {
+    for (int i = 0; i < edge_count - 1; i++) {
+        for (int j = 0; j < edge_count - i - 1; j++) {
             if (e[j].distance > e[j + 1].distance) {
                 temp = e[j];
                 e[j] = e[j + 1];
@@ -47,44 +40,35 @@ void sort() {
 }
 
 int main() {
-    int i, total, j, k, m, n, edgeselected = 0, nodel, noder;
+    int total, edgeselected = 0;
 
     printf("\nEnter the number of nodes: ");
     scanf("%d", &total);
 
-    for (i = 0; i < total; i++)
-        getdata(i, total);
-
+    for (int i = 0; i < total; i++) getdata(i, total);
     init(total);
     sort();
 
     printf("\nThe Sorted order of edges:");
-    for (i = 0; i < edge_count; i++)
+    for (int i = 0; i < edge_count; i++)
         printf("\nEdge: %d First node: %c Second node: %c Distance: %d",
                i, e[i].first_node + 65, e[i].second_node + 65, e[i].distance);
 
-    i = 0;
-    do {
-        e[i].selected = 1;
-        nodel = e[i].first_node;
-        noder = e[i].second_node;
+    for (int i = 0; edgeselected < total - 1; i++) {
+        int nodel = e[i].first_node, noder = e[i].second_node;
 
-        if (node[nodel].set == node[noder].set)
-            e[i].selected = -1;
-        else {
+        if (node[nodel].set != node[noder].set) {
+            e[i].selected = 1;
             edgeselected++;
-            m = node[nodel].set;
-            k = node[noder].set;
-            for (n = 0; n < total; n++) {
-                if (node[n].set == k)
-                    node[n].set = m;
+            int m = node[nodel].set, k = node[noder].set;
+            for (int n = 0; n < total; n++) {
+                if (node[n].set == k) node[n].set = m;
             }
         }
-        i++;
-    } while (edgeselected < (total - 1));
+    }
 
     printf("\nMinimum Spanning Tree is:");
-    for (i = 0; i < edge_count; ++i) {
+    for (int i = 0; i < edge_count; i++) {
         if (e[i].selected == 1)
             printf("\n%c <-------> %c Distance %d",
                    e[i].first_node + 65, e[i].second_node + 65, e[i].distance);
